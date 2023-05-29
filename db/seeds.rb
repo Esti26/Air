e2e940d308012191d9718271b5bcbb6da184dce9
madhavafranchin <madhavafranchin@gmail.com>
2023-05-29 17:02:01 +0200

    adds seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  User.create(email: Faker::Internet.email, password: "12345")
+end
+
+30.times do
+  Wig.create(color: Faker::Color.color_name, length: Faker::Measurement.length)
+end
