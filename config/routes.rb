541b456457b73df296c6fd2ca490f9ab1d2f29ad
madhavafranchin <madhavafranchin@gmail.com>
2023-05-29 15:37:41 +0200

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :wigs do
+    resources :wig_rentals, only: %i[new create]
+  end
 end
