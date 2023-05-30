3ea2fbb7071571905fa29c4bcfdd9837e7e5aae9
William Dela Rosa <williamgdelarosa1220@gmail.com>
2023-05-30 17:01:47 +0200

    Resolved wigs_rental_controller and view

--- app/controllers/wig_rentals_controller.rb
+++ app/controllers/wig_rentals_controller.rb
@@ -5,19 +5,23 @@ class WigRentalsController < ApplicationController
   end
 
   def create
+    # raise
     @wig = Wig.find(params[:wig_id])
     @wig_rental = WigRental.new(wig_rental_params)
-    @wig_rental.wig_id = @wig
+    @wig_rental.user = current_user
+    @wig_rental.wig= @wig
     if @wig_rental.save
-      redirect_to wigs_path
+      redirect_to wig_path(@wig)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+
+
   private
 
   def wig_rental_params
-    params.require(:wig_rental).permit(:user_id, :rental_start, :rental_end)
+    params.require(:wig_rental).permit(:user_id, :rental_start, :rental_end, :wig_id)
   end
 end
