de6d325557e0e52e3e6a49cb6ef491458025bc3c
William Dela Rosa <williamgdelarosa1220@gmail.com>
2023-05-30 15:30:39 +0200

    Partial wig-rental

--- app/controllers/wig_rentals_controller.rb
+++ app/controllers/wig_rentals_controller.rb
@@ -1,11 +1,11 @@
 class WigRentalsController < ApplicationController
   def new
-    @wig = Wig.find(params[:id])
+    @wig = Wig.find(params[:wig_id])
     @wig_rental = WigRental.new
   end
 
   def create
-    @wig = Wig.find(params[:id])
+    @wig = Wig.find(params[:wig_id])
     @wig_rental = WigRental.new(wig_rental_params)
     @wig_rental.wig_id = @wig
     if @wig_rental.save
