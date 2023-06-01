8ea389e903c2da96a6052a0a0cc8445b82d17b40
Estibaliz Lorenzo <esti_2612@hotmail.com>
2023-06-01 16:52:43 +0200

    adds meaningful seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,14 @@ User.delete_all
 10.times do
   User.create!(email: Faker::Internet.email, password: "123456")
 end
-colors = ["multi-color", "white", "blue", "blonde", "blonde", "blue", "purple", "gray", "pink", "green"]
+colors = ["multi-color", "white", "blue", "blonde", "brown", "blue", "purple", "gray", "pink", "green"]
 
 images = [
   "https://images.unsplash.com/photo-1595888433198-fc3fba4d48e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80.jpg",
   "https://images.unsplash.com/photo-1593880223042-744ce9a4b58f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
   "https://images.unsplash.com/photo-1559564071-dfa53d83b513?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80",
   "https://images.unsplash.com/photo-1579584707409-58eaaa0e9584?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80",
-  "https://images.unsplash.com/photo-1481044090193-9061560f70d9?ixlib=rb-4.0.3&ixid=M3wxM[…]dlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2074&q=80",
+  "https://images.unsplash.com/photo-1643818622704-44d9ccd28c5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=754&q=80",
   "https://images.unsplash.com/photo-1589738216835-c8de9d5fb452?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=326&q=80",
   "https://plus.unsplash.com/premium_photo-1684677942189-957a4b918979?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
   "https://images.unsplash.com/photo-1574773004910-1eeaabb62b55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80",
