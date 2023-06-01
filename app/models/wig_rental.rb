b8231dd1fe754b3d34780317f92d4c0a9b4c76fd
Estibaliz Lorenzo <esti_2612@hotmail.com>
2023-06-01 12:42:25 +0200

    adds logic to the rental calculations

--- app/models/wig_rental.rb
+++ app/models/wig_rental.rb
@@ -1,4 +1,17 @@
 class WigRental < ApplicationRecord
   belongs_to :user
   belongs_to :wig
+
+  validates :rental_start, :rental_end, presence: true
+  validate :rental_end_after_rental_start
+
+  private
+
+  def rental_end_after_rental_start
+    return if rental_end.blank? || rental_start.blank?
+
+    if rental_end < rental_start
+      errors.add(:rental_end, "must be after the start date")
+    end
+  end
 end
